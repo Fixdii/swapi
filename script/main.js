f1d26f0b96167949ebf207ed326855b87cf39583
Fixdii <walera2001@gmail.com>
2021-11-30 00:25:09 +0300

    improved pagination, changed display of information about a person

--- script/main.js
+++ script/main.js
@@ -1,174 +1,240 @@
-const PEOPLE_BY_PAGE_URL = "https://swapi.dev/api/people/?page=1";
-const SEARCH_POPLE_BY_NAME_URL = 'https://swapi.dev/api/people/?search=';
+const PEOPLE_BY_PAGE_URL = "https://swapi.dev/api/people/";
 
 const buttonLoad = document.querySelector(".button-load");
 const content = document.querySelector(".content-load");
+const page = document.querySelector(".page");
 
-let nextURL = '';
-let previosURL = '';
+let searchInput;
+
+let nextURL = "";
+let previousURL = "";
+let allPages = 0;
 
 function getPage(url) {
-  const query = url.split('?')[1];
-  const page = query 
+  const query = url.split("?")[1];
+  const page = query
     ? query
-      .split('&')
-        .find((elem, i, arr) => elem.indexOf('page') !== -1)
-      ?.split('=')[1] 
-      || 1
+        .split("&")
+        .find((elem, i, arr) => elem.indexOf("page") !== -1)
+        ?.split("=")[1] || 1
     : 1;
-
   return +page;
 }
 
+function countPage(allPages, currentPage) {
+  page.textContent =
+    "page" +
+    " " +
+    currentPage +
+    " " +
+    "of" +
+    "  " +
+    Math.ceil(allPages.count / 10);
+}
+
+function getSearch(url) {
+  const query = url.split("?")[1];
+  const search = query
+    ? query
+        .split("&")
+        .find((elem, i, arr) => elem.indexOf("search") !== -1)
+        ?.split("=")[1] || ""
+    : "";
+
+  return search;
+}
+
 function sendRequest(url) {
-  const method = 'GET';
+  const method = "GET";
 
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
+  return fetch(url, { method }).then((res) => {
+    const searchQuery = getSearch(res.url);
 
-    xhr.onload = () => {
-      if (xhr.status >= 400) {
-        reject(xhr.response);
-      } else {
-        const people = JSON.parse(xhr.response);
-
-        nextURL = people.next;
-        previosURL = people.previous;
-
-        resolve(people);
+    if (searchQuery) {
+      if (searchQuery !== searchInput.value) {
+        throw new Error("wrongSearch");
       }
-    };
+    }
 
-    xhr.onerror = () => {
-      reject("Запрос не удался");
-    };
-
-    xhr.send();
+    return res.json();
   });
 }
 
+function setNext(nextUrl = nextURL) {
+  const buttonNext = document.getElementById("button-next");
+  nextURL = nextUrl;
+
+  if (!buttonNext) {
+    return;
+  }
+
+  if (!nextUrl) {
+    buttonNext.disabled = true;
+  } else {
+    buttonNext.disabled = false;
+  }
+}
+
+function setPrev(prevUrl = previousURL) {
+  const buttonPrevious = document.getElementById("button-previous");
+  previousURL = prevUrl;
+
+  if (!buttonPrevious) {
+    return;
+  }
+
+  if (!prevUrl) {
+    buttonPrevious.disabled = true;
+  } else {
+    buttonPrevious.disabled = false;
+  }
+}
 
 function loadContent(people, page) {
-    const divForDelete = content.firstChild;
-    divForDelete && divForDelete.remove();
-    addContent(people, page);
+  setNext(people.next);
+  setPrev(people.previous);
+
+  allPages = people.count;
+
+  const divForDelete = content.firstChild;
+  if(divForDelete){
+    divForDelete.remove();
   }
 
-  function addContent(people, page) {
-    let div = document.createElement("div");
-    let ol = document.createElement("ol");
-    ol.start = "6";
-    ol.className = "rounded";
-    div.append(ol);
-    content.append(div);
+  addContent(people, page);
+}
 
-    people.results.forEach((item, i) => {
-      let li = document.createElement("li");   
+function addContent(people, page) {
+  let div = document.createElement("div");
+  let ol = document.createElement("ol");
+  ol.className = "rounded";
+  div.append(ol);
+  content.append(div);
 
-      if (i === 0) {
+  people.results.forEach((item, i) => {
+    let li = document.createElement("li");
 
-        li.style=`counter-reset: li ${(page * 10) - 10};`;
-      }
-      let a = document.createElement("a");
-      a.href = "#";
-      a.append(item.name);
-      li.append(a);
-      ol.append(li);
+    if (i === 0) {
+      li.style = `counter-reset: li ${page * 10 - 10};`;
+    }
+    let a = document.createElement("a");
+    a.href = "#";
+    a.append(item.name);
+    li.append(a);
+    ol.append(li);
 
-      a.addEventListener("click", (event) => {
-        event.preventDefault();
-        const name = event.target.innerHTML;
-        const ols = Array.prototype.slice.call(
-          document.querySelectorAll(".rounded")
-        );
-        const existOl = ols.find(
-          (element) => element.firstChild.innerHTML === `Name:${name}`
-        );
-
-        if (existOl) {
-          existOl.remove();
-        } else {
-          addInfo(people, li, item.name);
-        }
-      });
-    });
-
-    let buttonNext = document.createElement("button");
-    buttonNext.className = "button-load";
-    buttonNext.append("Next");
-    buttonNext.style.float = "right";
-    let buttonPrevious = document.createElement("button");
-    buttonPrevious.className = "button-load";
-    buttonPrevious.append("Previous");
-    buttonPrevious.style.float = "left";
-    div.append(buttonNext);
-    div.append(buttonPrevious);
-
-    buttonNext.addEventListener("click", (event) => {
+    a.addEventListener("click", (event) => {
       event.preventDefault();
-      pageCheck(true);
-    });
-    buttonPrevious.addEventListener("click", (event) => {
-      event.preventDefault();
-      pageCheck(false);
-    });
-  }
+      const name = event.target.innerHTML;
+      const ols = Array.prototype.slice.call(
+        document.querySelectorAll(".rounded")
+      );
 
-  function addInfo(people, li, name) {
-    let ol2 = document.createElement("ol");
-    ol2.className = "rounded info";
-    li.append(ol2);
+      const existOl = ols.find(
+        (element) => element.firstChild.innerHTML === `Name: ${name}`
+      );
 
-    people.results.forEach((item) => {
-      if (item.name == name) {
-        for (const key of Object.keys(item)) {
-          let li2 = document.createElement("li");
-          li2.append(key[0].toUpperCase() + key.slice(1) + ":" + item[key]);
-          ol2.append(li2);
+      if (existOl) {
+        existOl.remove();
+      } else {
+        addInfo(people, li, item.name);
+      }
+    });
+  });
+
+  let buttonNext = document.createElement("button");
+  buttonNext.className = "button-load";
+  buttonNext.id = "button-next";
+  buttonNext.append("Next");
+  buttonNext.style.float = "right";
+  let buttonPrevious = document.createElement("button");
+  buttonPrevious.className = "button-load";
+  buttonPrevious.append("Previous");
+  buttonPrevious.id = "button-previous";
+  buttonPrevious.style.float = "left";
+  div.append(buttonNext);
+  div.append(buttonPrevious);
+
+  buttonNext.addEventListener("click", (event) => {
+    event.preventDefault();
+    pageCheck(true, buttonNext);
+  });
+  buttonPrevious.addEventListener("click", (event) => {
+    event.preventDefault();
+    pageCheck(false, buttonPrevious);
+  });
+
+  setNext();
+  setPrev();
+}
+
+function addInfo(people, li, name) {
+  let ol2 = document.createElement("ol");
+
+  ol2.className = "rounded info";
+  li.append(ol2);
+
+  people.results.forEach((item) => {
+    if (item.name == name) {
+      for (const key of Object.keys(item)) {
+        if (key == "gender") {
+          return;
         }
-      }
-    });
-  }
-
-  function pageCheck(isNext) {
-    if (isNext) {
-      if (!!nextURL) {
-        loadPageContent(nextURL);
-      } else {
-        alert("Список окончен");
-      }
-    } else {
-      if (!!previosURL) {
-        loadPageContent(previosURL);
-      } else {
-        alert("Список окончен");
+        let li2 = document.createElement("li");
+        li2.append(
+          key[0].toUpperCase() + key.slice(1).split("_")[0] + ": " + item[key]
+        );
+        ol2.append(li2);
       }
     }
-  }
+  });
+}
 
+function pageCheck(isNext, btn) {
+  if (isNext) {
+    if (!!nextURL) {
+      loadPageContent(nextURL);
+    } else {
+    }
+  } else {
+    if (!!previousURL) {
+      loadPageContent(previousURL);
+    } else {
+    }
+  }
+}
 
 function init() {
   loadPageContent();
 
-  const searchInput = document.getElementById("searchInput");
-  const searchButton = document.getElementById("search");
+  searchInput = document.getElementById("searchInput");
 
-  searchButton.addEventListener("click", (event) => {
-    const requestURL = `${SEARCH_POPLE_BY_NAME_URL}${searchInput.value}`;
+  searchInput.addEventListener("input", (event) => {
+    const requestURL = `${PEOPLE_BY_PAGE_URL}?search=${searchInput.value}`;
     const page = getPage(requestURL);
+
     sendRequest(requestURL)
-      .then((people) => loadContent(people, page))
-      .catch((err) => console.error(err));
-  })
+      .then((people) => {
+        loadContent(people, page);
+        countPage(people, page);
+      })
+      .catch((err) => {
+        if (err.message !== "wrongSearch") {
+          console.error(err);
+        }
+      });
+  });
 }
 
 function loadPageContent(url = PEOPLE_BY_PAGE_URL) {
   const page = getPage(url);
 
   sendRequest(url)
-    .then((people) => loadContent(people, page))
+    .then((people) => {
+      loadContent(people, page);
+      countPage(people, page);
+    })
     .catch((err) => console.error(err));
 }
 
-window.addEventListener('DOMContentLoaded', () => init());
+window.addEventListener("DOMContentLoaded", () => init());
